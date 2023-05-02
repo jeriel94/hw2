44511f63072bb4d4a5bf56f1a98df603567b734c
Jeriel Acosta <jeriel.acosta@kellogg.northwestern.edu>
2023-05-02 18:03:43 +0000

    movies insert

--- db/migrate/20230501185317_create_movies.rb
+++ db/migrate/20230501185317_create_movies.rb
@@ -2,8 +2,8 @@ class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
       t.string "title"
-      t.integer "year"
-      t.string "rating"
+      t.integer "year_released"
+      t.string "rated"
       t.integer "studio_id"
 
       t.timestamps
--- db/migrate/20230501204144_create_roles.rb
+++ db/migrate/20230501204144_create_roles.rb
@@ -4,6 +4,7 @@ class CreateRoles < ActiveRecord::Migration[7.0]
       t.integer "movie_id"
       t.integer "actor_id"
       t.string "character_name"
+      
       t.timestamps
     end
   end
